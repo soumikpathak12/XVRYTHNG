3e87d90d02096e409627b8dad8601feee4c6721e
hiimintt <nlee0075@student.monash.edu>
2026-02-19 09:14:26 +0700

    [Add] Update database

--- database/leads.sql
+++ database/leads.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 12, 2026 at 05:17 AM
+-- Generation Time: Feb 19, 2026 at 03:11 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -31,6 +31,8 @@ CREATE TABLE `leads` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `stage` enum('new','contacted','qualified','inspection_booked','inspection_completed','proposal_sent','negotiation','closed_won','closed_lost') NOT NULL,
   `customer_name` varchar(150) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` varchar(50) NOT NULL,
   `suburb` varchar(150) DEFAULT NULL,
   `system_size_kw` decimal(10,2) DEFAULT NULL,
   `value_amount` decimal(14,2) DEFAULT NULL,
@@ -41,24 +43,37 @@ CREATE TABLE `leads` (
   `last_activity_at` datetime DEFAULT NULL,
   `site_inspection_date` datetime DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
+  `system_type` varchar(100) DEFAULT NULL,
+  `house_storey` varchar(50) DEFAULT NULL,
+  `roof_type` varchar(100) DEFAULT NULL,
+  `meter_phase` varchar(20) DEFAULT NULL,
+  `access_to_second_storey` tinyint(1) DEFAULT NULL,
+  `access_to_inverter` tinyint(1) DEFAULT NULL,
+  `pre_approval_reference_no` varchar(100) DEFAULT NULL,
+  `energy_retailer` varchar(120) DEFAULT NULL,
+  `energy_distributor` varchar(120) DEFAULT NULL,
+  `solar_vic_eligibility` tinyint(1) DEFAULT NULL,
+  `nmi_number` varchar(50) DEFAULT NULL,
+  `meter_number` varchar(50) DEFAULT NULL,
+  `contacted_at` datetime DEFAULT NULL,
+  `last_inbound_email_at` datetime DEFAULT NULL,
+  `last_outbound_email_at` datetime DEFAULT NULL,
+  `followup_first_sent_at` datetime DEFAULT NULL,
+  `followup_second_sent_at` datetime DEFAULT NULL,
+  `flagged_for_review_at` datetime DEFAULT NULL,
+  `auto_close_nonresponsive` tinyint(1) NOT NULL DEFAULT 0,
+  `lost_reason` varchar(120) DEFAULT NULL,
+  `owner_doc_last_sent_at` datetime DEFAULT NULL,
+  `owner_doc_reminders_count` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `leads`
 --
 
-INSERT INTO `leads` (`id`, `stage`, `customer_name`, `suburb`, `system_size_kw`, `value_amount`, `source`, `is_closed`, `is_won`, `won_lost_at`, `last_activity_at`, `site_inspection_date`, `created_at`, `updated_at`) VALUES
-(1, 'new', 'test', 'test', 1000.00, NULL, NULL, 0, 0, NULL, '2026-02-11 08:52:35', NULL, '2026-02-11 01:52:35', NULL),
-(2, 'new', 'test', 'test', 1000.00, NULL, NULL, 0, 0, NULL, '2026-02-11 08:52:45', NULL, '2026-02-11 01:52:45', NULL),
-(3, 'new', 'test', 'test', 100.00, 200.00, 'test', 0, 0, NULL, '2026-02-11 08:55:40', NULL, '2026-02-11 01:55:40', NULL),
-(4, 'closed_won', 'test', 'test', 100.00, 123.00, '123', 1, 1, '2026-02-11 09:27:31', '2026-02-11 09:27:31', NULL, '2026-02-11 01:55:57', '2026-02-11 02:27:31'),
-(5, 'new', 'test', 'test', 100.00, 100.00, 'Web', 0, 0, NULL, '2026-02-11 09:45:20', NULL, '2026-02-11 02:45:20', NULL),
-(6, 'new', 'Minh', 'Minh', 5.00, 5.00, 'gg', 0, 0, NULL, '2026-02-12 06:53:36', NULL, '2026-02-11 02:46:32', '2026-02-11 23:53:36'),
-(7, 'new', 'testcal', 'cal', 100000.00, 5000.00, 'Web', 0, 0, NULL, '2026-02-12 08:05:59', NULL, '2026-02-12 01:05:59', NULL),
-(8, 'new', 'adsfsa', 'safsdfasdf', 50.00, 50.00, 'wewerwrwerwer', 0, 0, NULL, '2026-02-12 08:07:55', NULL, '2026-02-12 01:07:55', NULL),
-(9, 'new', 'testfwewefwe', 'set', 7.00, 50.00, 'fwefwefwefewfwe', 0, 0, NULL, '2026-02-12 08:09:00', NULL, '2026-02-12 01:09:00', NULL),
-(10, 'new', 'testagain', 'aeokne', 50.00, 500.00, 'werwer', 0, 0, NULL, '2026-02-12 08:15:31', '2026-02-14 09:15:00', '2026-02-12 01:15:31', NULL);
+INSERT INTO `leads` (`id`, `stage`, `customer_name`, `email`, `phone`, `suburb`, `system_size_kw`, `value_amount`, `source`, `is_closed`, `is_won`, `won_lost_at`, `last_activity_at`, `site_inspection_date`, `created_at`, `updated_at`, `system_type`, `house_storey`, `roof_type`, `meter_phase`, `access_to_second_storey`, `access_to_inverter`, `pre_approval_reference_no`, `energy_retailer`, `energy_distributor`, `solar_vic_eligibility`, `nmi_number`, `meter_number`, `contacted_at`, `last_inbound_email_at`, `last_outbound_email_at`, `followup_first_sent_at`, `followup_second_sent_at`, `flagged_for_review_at`, `auto_close_nonresponsive`, `lost_reason`, `owner_doc_last_sent_at`, `owner_doc_reminders_count`) VALUES
+(1, 'new', 'MInwerwe', '', '', 'test', 1000.00, NULL, NULL, 0, 0, NULL, '2026-02-19 08:50:49', NULL, '2026-02-11 01:52:35', '2026-02-19 01:50:49', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, NULL, '2026-02-19 08:50:49', 22);
 
 --
 -- Indexes for dumped tables
@@ -84,7 +99,7 @@ ALTER TABLE `leads`
 -- AUTO_INCREMENT for table `leads`
 --
 ALTER TABLE `leads`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
