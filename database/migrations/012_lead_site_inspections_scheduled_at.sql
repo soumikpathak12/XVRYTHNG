bcb4b5427ac1ecd81958dfdf5566dd5120490f61
hiimintt <nlee0075@student.monash.edu>
2026-03-15 23:03:35 +1100

    SQL chema for inspection

--- database/migrations/012_lead_site_inspections_scheduled_at.sql
+++ database/migrations/012_lead_site_inspections_scheduled_at.sql
@@ -1,5 +1,2 @@
--- Store scheduled inspection date/time on lead_site_inspections (in addition to leads.site_inspection_date).
--- Run in phpMyAdmin: select database → SQL tab → run the statement below.
-
 ALTER TABLE lead_site_inspections
   ADD COLUMN scheduled_at DATETIME DEFAULT NULL AFTER inspector_name;
